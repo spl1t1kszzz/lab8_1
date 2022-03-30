3156f9522ea573324c8859657ec11a80c18aa463
spl1t1kszzz <nik17403@gmail.com>
2022-03-30 23:31:44 +0700

    add makeSet and find

--- functions.h
+++ functions.h
@@ -1,8 +1,21 @@
-//
-// Created by Никита on 30.03.2022.
-//
 
 #ifndef LAB8_2_FUNCTIONS_H
 #define LAB8_2_FUNCTIONS_H
+
+// This function makes set.
+void makeSet(int* array, int* ranks, int value ) {
+    *(array + value) = value;
+    *(ranks + value) = 0;
+}
+
+
+//
+int find(const int *array, int value) {
+    if (value == *(array + value))
+        return value;
+    return find(array, *(array + value));
+}
+
+
 
 #endif //LAB8_2_FUNCTIONS_H
--- main.c
+++ main.c
@@ -2,9 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "functions.h"
+#define maxLen (INT32_MAX)
 
 
 int main() {
-    printf("%s", "Hello world!");
+    int* sets = malloc(sizeof (int) * 11);
+    int* ranks = malloc(sizeof (int) * 11);
+    makeSet(sets, 1);
+    for (int i = 1; i < 11; ++i)
+        *(sets + i) = i;
+    for (int i = 1; i < 11; ++i) {
+        printf("%d ", *(sets + i));
+
+    }
     return 0;
 }
