af92efea9439c7a80d4f6249cf123c665df1bfe2
spl1t1kszzz <nik17403@gmail.com>
2022-03-30 22:51:56 +0700

    create functions.h

---
+++ functions.h
@@ -0,0 +1,8 @@
+//
+// Created by Никита on 30.03.2022.
+//
+
+#ifndef LAB8_2_FUNCTIONS_H
+#define LAB8_2_FUNCTIONS_H
+
+#endif //LAB8_2_FUNCTIONS_H
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "functions.h"
 
 
 int main() {
